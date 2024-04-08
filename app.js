5a149b0f3a064fad88e9766af24fd154bcc7f863
raman kumar sharma <mr.ramansharma2000@gmail.com>
2024-04-08 21:20:58 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
+// const express = require('express');
+// const cors = require('cors');
+// const dotenv = require('dotenv');
+// const routes = require('./routes/routes');
+// const connectDB = require('./db/db');
+
+// dotenv.config(); // Load environment variables from .env file
+
+// const app = express();
+
+// // Middleware
+// app.use(cors());
+// app.use(express.json());
+
+// // Connect to MongoDB
+// connectDB();
+
+// // Routes
+// app.use('/api', routes);
+
+// // Server
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server started in development mode on port ${PORT}`));
+
+
+
+
+
 const express = require('express');
 const dotenv = require('dotenv');
 const routes = require('./routes/routes');
@@ -18,4 +46,4 @@ app.use('/api', routes);
 
 // Server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started in development mode on port ${PORT}`));
